8eaa1e0594683408bf2a85251e27cd95939305bc
Vinicius Oliveira <vr.sena@gmail.com>
2019-05-06 12:49:35 -0400

    Made a grid with snapping and movement of a cube.

---
+++ Assets/Scripts/CubePlacer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class CubePlacer : MonoBehaviour
+{
+    private Grid grid;
+    public GameObject cube;
+
+    private void Awake()
+    {
+        grid = FindObjectOfType<Grid>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                PlaceCubeNear(hitInfo.point);
+            }
+        }
+    }
+
+    private void PlaceCubeNear(Vector3 clickPoint)
+    {
+        var finalPosition = grid.GetNearestPointOnGrid(clickPoint);
+        cube.transform.position = Vector3.MoveTowards(cube.transform.position, finalPosition, Time.deltaTime);
+        //GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosition;
+
+        //GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = finalPosition;
+    }
+}
