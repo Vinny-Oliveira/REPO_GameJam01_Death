fa5ecc3dfbfe68d340bb4fa52d635d6a2d407250
Vinicius Oliveira <vr.sena@gmail.com>
2019-05-07 17:24:57 -0400

    DoTween test

---
+++ Assets/Scripts/Test Scripts/DoTweenController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DoTweenController : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 targetLocation = Vector3.zero;
+
+    [Range(1f, 10f), SerializeField]
+    private float moveDuration = 1f;
+
+    [SerializeField]
+    private Ease moveEase = Ease.Linear;
+
+    enum DoTweenType {MoveOneWay}
+
+    [SerializeField]
+    DoTweenType dtType = DoTweenType.MoveOneWay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (dtType == DoTweenType.MoveOneWay) {
+            if (targetLocation == Vector3.zero) {
+                targetLocation = transform.position;
+            }
+
+            transform.DOMove(targetLocation,moveDuration);
+        }
+    }
+    
+}
