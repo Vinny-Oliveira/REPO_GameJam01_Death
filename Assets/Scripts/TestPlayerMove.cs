6fc13fba3dba78a52d05afe37c1ee73080e8f829
Vinicius Oliveira <vr.sena@gmail.com>
2019-05-07 10:37:37 -0400

    Movement of the player and orientation fixed.

--- Assets/Scripts/TestPlayerMove.cs
+++ Assets/Scripts/TestPlayerMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TestPlayerMove : MonoBehaviour
 {
-
+    [SerializeField]
     float fltScaler = 1f;
 
     // Start is called before the first frame update
@@ -16,23 +16,22 @@ public class TestPlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Player's Movement
         if(Input.GetKeyDown(KeyCode.W))
         {
-            //transform.position += Vector3.left;
-            transform.position += Vector3.forward;
+            transform.position += Vector3.forward * fltScaler;
         }
         else if (Input.GetKeyDown(KeyCode.A))
         {
-            //transform.position += Vector3.back;
-            transform.position += Vector3.back;
+            transform.position += Vector3.left * fltScaler;
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
-            transform.position += Vector3.right;
+            transform.position += Vector3.back * fltScaler;
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
-            transform.position += Vector3.forward;
+            transform.position += Vector3.right * fltScaler;
         }
     }
 }
