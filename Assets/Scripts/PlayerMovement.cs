a9e3f898812be7823edadb55124ac7e94971f9be
Vinicius Oliveira <vr.sena@gmail.com>
2019-05-08 13:55:31 -0400

    Game over panel is triggered.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -22,34 +22,32 @@ public class PlayerMovement : MonoBehaviour
             // Player's Movement
             if ((Input.GetKeyDown(KeyCode.W)) && (transform.position.z < furthestCorner.transform.position.z))
             {
-                GameManager.GetInstance().MovementTween(gameObject, Vector3.forward, moveEase);
-                transform.DORotateQuaternion(Quaternion.LookRotation(Vector3.forward), GameManager.ROTATION_DURATION);
-                npcs.BroadcastMessage("MakeNPCMove");
+                MakePlayerMove(Vector3.forward);
             }
             else if (Input.GetKeyDown(KeyCode.A) && (transform.position.x > originCorner.transform.position.x))
             {
-                GameManager.GetInstance().MovementTween(gameObject, Vector3.left, moveEase);
-                transform.DORotateQuaternion(Quaternion.LookRotation(Vector3.left), GameManager.ROTATION_DURATION);
-                npcs.BroadcastMessage("MakeNPCMove");
+                MakePlayerMove(Vector3.left);
             }
             else if (Input.GetKeyDown(KeyCode.S) && (transform.position.z > originCorner.transform.position.z))
             {
-                GameManager.GetInstance().MovementTween(gameObject, Vector3.back, moveEase);
-                transform.DORotateQuaternion(Quaternion.LookRotation(Vector3.back), GameManager.ROTATION_DURATION);
-                npcs.BroadcastMessage("MakeNPCMove");
+                MakePlayerMove(Vector3.back);
             }
             else if (Input.GetKeyDown(KeyCode.D) && (transform.position.x < furthestCorner.transform.position.x))
             {
-                GameManager.GetInstance().MovementTween(gameObject, Vector3.right, moveEase);
-                transform.DORotateQuaternion(Quaternion.LookRotation(Vector3.right), GameManager.ROTATION_DURATION);
-                npcs.BroadcastMessage("MakeNPCMove");
+                MakePlayerMove(Vector3.right);
             }
         }
         
     }
 
-    void MakePlayerMove() {
-
+    /// <summary>
+    /// Make the player move and rotate in the proper directions using DoTween
+    /// </summary>
+    /// <param name="inDirection"></param>
+    void MakePlayerMove(Vector3 inDirection) {
+        GameManager.GetInstance().MovementTween(gameObject, inDirection, moveEase);
+        transform.DORotateQuaternion(Quaternion.LookRotation(inDirection), GameManager.ROTATION_DURATION);
+        npcs.BroadcastMessage("MakeNPCMove");
     }
 
     /// <summary>
@@ -58,14 +56,11 @@ public class PlayerMovement : MonoBehaviour
     /// <param name="collision"></param>
     private void OnTriggerEnter(Collider collision) {
         if (collision.CompareTag("NPC")) {
-            
-            //GameManager.GetInstance().intInnocentsKilled++;
             StartCoroutine(GameManager.GetInstance().LateCall());
             // Trigger score decrease or whatever else is supposed to happen
         } else if (collision.gameObject.CompareTag("Target")) {
             Debug.Log("Target eliminated.");
-            GameManager.GetInstance().isGameOver = true;
-            // Trigger game over panel
+            GameManager.GetInstance().TriggerGameOver();
         }
 
         Destroy(collision.gameObject);
