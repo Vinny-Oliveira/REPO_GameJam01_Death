7084242d9b6186cb11ae965dd262a40533162780
Amanda <amandaduarte2012@gmail.com>
2019-05-07 21:05:41 -0300

    Created Scene_Amanda with the Menus
    Created my scene with the Main and Pause Menus and the scene for the End-Game. Added the transitions between panels and a Exit game button.
    There're all templates, since some buttons don't work because they are not synced with the code yet (like the Play or Resume Game button).

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void ExitGame()  // AMANDA CREATED
+    {
+        Application.Quit();
+        Debug.Log("Exiting");
+    }
+
+}
