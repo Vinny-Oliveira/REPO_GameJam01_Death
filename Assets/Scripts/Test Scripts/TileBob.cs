b807c506cefb0295a09e59f5db7f425d599d9779
FTWRahul <r.poundarik@gmail.com>
2019-05-09 21:16:25 -0400

    Repo Fixes

--- Assets/Scripts/Test Scripts/TileBob.cs
+++ Assets/Scripts/Test Scripts/TileBob.cs
@@ -5,8 +5,11 @@ using DG.Tweening;
 
 public class TileBob : MonoBehaviour
 {
+    GameObject player;
+
     private void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         transform.localScale = new Vector3(0, 0, 0);
         StartCoroutine(ScaleUp());
     }
@@ -21,8 +24,18 @@ public class TileBob : MonoBehaviour
     [ContextMenu("Tween!")]
     void TweenExpand()
     {
-        //transform.DOScale(.7f, 0.2f).onComplete(TweenBack);
-        transform.DOShakeScale(0.25f, new Vector3(0.7f, .3f, .7f), 0, 0, false);
-        //transform.DOPunchScale(new Vector3(.5f, .2f, 0.5f), 0.25f, 1, 0);
+        if ((transform.position - player.transform.position).magnitude < 3)
+        {
+            Sequence mySequence = DOTween.Sequence();
+            mySequence.Prepend(transform.DOScale(new Vector3(Random.Range(4, 4.5f), .5f, Random.Range(4, 4.5f)), Random.Range(.1f, .5f)).SetEase(Ease.OutBack));
+            //mySequence.Prepend(transform.DOShakeScale(0.25f, new Vector3(Random.Range(0.2f, 0.7f), .3f, Random.Range(0.2f, 0.7f)), 0, 0, false));
+            mySequence.Append(transform.DOScale(new Vector3(5, 1, 5), Random.Range(.1f, .5f)).SetEase(Ease.OutBack));
+            mySequence.Play();
+
+            //transform.DOScale(.7f, 0.2f).onComplete(TweenBack);
+            //transform.DOShakeScale(0.25f, new Vector3(0.7f, .3f, .7f), 0, 0, false);
+            //transform.DOPunchScale(new Vector3(.5f, .2f, 0.5f), 0.25f, 1, 0);
+        }
+
     }
 }
